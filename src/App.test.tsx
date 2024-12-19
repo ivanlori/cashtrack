63954ab5bac089c175911d5f4a0fc504b7f80d16
Ivan Lori <ivan.lori@protonmail.com>
2024-12-19 14:14:11 +0100

    test: adds basic unit test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,12 +1,34 @@
-import { render } from '@testing-library/react';
-import { BrowserRouter } from 'react-router-dom'; // Add this import statement
+import { render, screen, waitFor } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import userEvent from '@testing-library/user-event';
+import selectEvent from 'react-select-event';
 import App from './App';
 
-test('should match snapshot', () => {
-	const { baseElement } = render(
+// Polyfill for structuredClone
+if (typeof structuredClone === 'undefined') {
+	global.structuredClone = (obj: any) => JSON.parse(JSON.stringify(obj));
+}
+
+test('should add a price item', async () => {
+	render(
 		<BrowserRouter>
 			<App />
 		</BrowserRouter>
 	);
-	expect(baseElement).toMatchSnapshot();
+	const input = screen.getByTestId('expense-input')
+	const categorySelect = screen.getByLabelText('Seleziona o crea una categoria')
+	const addBtn = screen.getByText('Aggiungi')
+
+	await userEvent.type(input, '100')
+	await waitFor(() => {
+		selectEvent.create(categorySelect, 'Food')
+	})
+	await waitFor(() => {
+		selectEvent.select(categorySelect, 'Crea categoria "Food"')
+	})
+	await userEvent.click(addBtn)
+
+	await waitFor(() => {
+		expect(screen.getByTestId('expense-item')).toHaveTextContent('Food -100€')
+	})
 })
